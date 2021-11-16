1bd64848d719ab883490ed63bd7687fab0b6870c
paragUn <84090039+paragUn@users.noreply.github.com>
2021-11-16 19:38:52 +0300

    Update Words.java

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -2,7 +2,6 @@ package com.efimchick.ifmo.streams.countwords;
 
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
